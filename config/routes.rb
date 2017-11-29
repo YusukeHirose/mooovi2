1fbf30e2910d9fca2028c27352140850904a864d
kdoebr21 <kodeberic@yahoo.co.jp>
2017-11-29 13:42:20 +0900

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,14 @@
 TechReviewSite::Application.routes.draw do
+
+  devise_for :users
+  resources :users, only: :show
+  resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
+    collection do #7つのアクション以外を使うときにcollection do〜endとする。
+      get 'search'
+    end
+  end
+  root 'products#index'
+
+
 end
